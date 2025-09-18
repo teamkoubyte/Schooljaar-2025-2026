34a55faf674f5440ec96ae693e5b624cdd4969fb
Mohamed Koubaa <mohamed.koubaa@bazandpoort.be>
2025-09-18 09:20:27 +0200

    Schoolbestanden

--- 05 - Software development/03 - Projecten/combinatieCombinatie2/src/main/java/com/example/combinatiecombinatie2/HelloApplication.java
+++ 05 - Software development/03 - Projecten/combinatieCombinatie2/src/main/java/com/example/combinatiecombinatie2/HelloApplication.java
@@ -5,11 +5,9 @@ import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.CornerRadii;
-import javafx.scene.layout.HBox;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -19,6 +17,8 @@ public class HelloApplication extends Application {
     public void start(Stage primaryStage) throws IOException {
         Scene scene;
         HBox root;
+        VBox vbox1, vbox2;
+        Circle cirkel;
 
         //*****************Hbox (De hoofdcontainer)*******************
         root = new HBox(20);
@@ -27,6 +27,16 @@ public class HelloApplication extends Application {
         root.setBackground(new Background(new BackgroundFill
                 (Color.ORANGE, new CornerRadii(0), new Insets(0))));
 
+        //*****************Vbox*****************
+        vbox1 = new VBox(10);
+        vbox1.setMinWidth(400);
+        vbox1.setPadding(new Insets(20));
+        vbox1.setAlignment(Pos.CENTER);
+        vbox1.setBackground(new Background(new BackgroundFill
+                (Color.BLUE, new CornerRadii(35), new Insets(20))));
+
+        //**************De kinderen van de HBOX********************
+        root.getChildren().add(vbox1);
         //*****************Scene*****************
         scene = new Scene(root, 320, 240);
         //*****************Primary Stage**********************
