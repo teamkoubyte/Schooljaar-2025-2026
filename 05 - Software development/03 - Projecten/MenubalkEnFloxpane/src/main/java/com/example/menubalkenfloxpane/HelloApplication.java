24573fe67a438247483c2d32398a4445d0f1c430
Mohamed Koubaa <mohamed.koubaa@bazandpoort.be>
2025-09-18 10:04:56 +0200

    Schoolbestanden

--- 05 - Software development/03 - Projecten/MenubalkEnFloxpane/src/main/java/com/example/menubalkenfloxpane/HelloApplication.java
+++ 05 - Software development/03 - Projecten/MenubalkEnFloxpane/src/main/java/com/example/menubalkenfloxpane/HelloApplication.java
@@ -3,6 +3,9 @@ package com.example.menubalkenfloxpane;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.MenuBar;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,7 +13,17 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage primaryStage) throws IOException {
+        Scene scene;
+        VBox root = new VBox(5);
+        MenuBar menubalk;
+        FlowPane flowPane;
+
+        //*********************Scene*************************
+        root.getChildren().addAll(menubalk, flowPane);
+        scene = new Scene(root, 400, 350);
+        //****************Primary Stage**********************
         primaryStage.setTitle("Hello!");
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 }
