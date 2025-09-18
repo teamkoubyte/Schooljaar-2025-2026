51aa59c9c967cda883f6f4a81daa9cb96f9c2c1a
Mohamed Koubaa <mohamed.koubaa@bazandpoort.be>
2025-09-18 09:09:56 +0200

    Schoolbestanden

--- 05 - Software development/03 - Projecten/combinatieCombinatie2/src/main/java/com/example/combinatiecombinatie2/HelloApplication.java
+++ 05 - Software development/03 - Projecten/combinatieCombinatie2/src/main/java/com/example/combinatiecombinatie2/HelloApplication.java
@@ -2,18 +2,37 @@ package com.example.combinatiecombinatie2;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) throws IOException {
+        Scene scene;
+        HBox root;
+
+        //*****************Hbox (De hoofdcontainer)*******************
+        root = new HBox(20);
+        root.setAlignment(Pos.CENTER);
+
+        root.setBackground(new Background(new BackgroundFill
+                (Color.ORANGE, new CornerRadii(0), new Insets(0))));
+
+        //*****************Scene*****************
+        scene = new Scene(root, 320, 240);
+        //*****************Primary Stage**********************
+        primaryStage.setTitle("Klassen en hun klastitularis");
+        primaryStage.setFullScreen(true);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
