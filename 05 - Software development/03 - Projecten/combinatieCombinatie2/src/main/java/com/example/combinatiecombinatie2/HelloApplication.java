cf61aefee17381c197a8b4e8c4e4925501ff9402
Mohamed Koubaa <mohamed.koubaa@bazandpoort.be>
2025-09-18 09:49:22 +0200

    Schoolbestanden

--- 05 - Software development/03 - Projecten/combinatieCombinatie2/src/main/java/com/example/combinatiecombinatie2/HelloApplication.java
+++ 05 - Software development/03 - Projecten/combinatieCombinatie2/src/main/java/com/example/combinatiecombinatie2/HelloApplication.java
@@ -5,9 +5,11 @@ import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -19,6 +21,8 @@ public class HelloApplication extends Application {
         HBox root;
         VBox vbox1, vbox2;
         Circle cirkel;
+        Label v1labelKlas,v1labelTitularis;
+        Label v2labelKlas, v2labelTitularis;
 
         //*****************Hbox (De hoofdcontainer)*******************
         root = new HBox(20);
@@ -27,7 +31,7 @@ public class HelloApplication extends Application {
         root.setBackground(new Background(new BackgroundFill
                 (Color.ORANGE, new CornerRadii(0), new Insets(0))));
 
-        //*****************Vbox*****************
+        //*****************Vbox1*****************
         vbox1 = new VBox(10);
         vbox1.setMinWidth(400);
         vbox1.setPadding(new Insets(20));
@@ -35,8 +39,34 @@ public class HelloApplication extends Application {
         vbox1.setBackground(new Background(new BackgroundFill
                 (Color.BLUE, new CornerRadii(35), new Insets(20))));
 
-        //**************De kinderen van de HBOX********************
-        root.getChildren().add(vbox1);
+        //*****************Vbox2*****************
+        vbox2 = new VBox(40);
+        vbox2.setMinWidth(250);
+        vbox2.setPadding(new Insets(20));
+        vbox2.setAlignment(Pos.CENTER);
+        vbox2.setBackground(new Background(new BackgroundFill
+                (Color.PINK, new CornerRadii(5), new Insets(20))));
+
+        //*****************Labels*****************
+        v1labelKlas = new Label("6IF&CW");
+        v1labelKlas.setTextFill(Color.WHITE);
+        v1labelKlas.setFont(new Font(25));
+        v1labelTitularis = new Label("Gerrit Wijns");
+        v1labelTitularis.setTextFill(Color.YELLOWGREEN);
+        v1labelTitularis.setFont(new Font(15));
+        v2labelKlas = new Label("6A&D");
+        v2labelTitularis = new Label("Steven Vermeulen");
+
+
+        //*****************Cirkel*****************
+        cirkel = new Circle(50);
+        cirkel.setFill(Color.FIREBRICK);
+
+        //**************De kinderen van de HBOX (root)********************
+        root.getChildren().addAll(vbox1, vbox2, cirkel);
+        //**************De kinderen van de VBOXEN********************
+        vbox1.getChildren().addAll(v1labelKlas, v1labelTitularis);
+        vbox2.getChildren().addAll(v2labelKlas, v2labelTitularis);
         //*****************Scene*****************
         scene = new Scene(root, 320, 240);
         //*****************Primary Stage**********************
