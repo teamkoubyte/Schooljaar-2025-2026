366c5620b635766628d6681f73c23fdf2b42ace5
Koubyte <info@koubyte.be>
2025-09-26 14:51:13 +0200

    School

---
+++ 07 - ITplus/02 - Opdrachten/02 - Python 02 - Het ultieme text-based game/hetUltiemeText-basedGame.py
@@ -0,0 +1,13 @@
+import colorama as color
+
+color.init(autoreset=True)
+
+spelkarakter = "voetballer"
+
+
+print ("Je bent een " + spelkarakter + " die zoveel mogelijk wil scoren.")
+print("Als je 10 keer hebt gescoord ga je door naar het volgende level.")
+
+naam = input("Dag voetballer. Hoe moet ik je noemen? ")
+lengte = input("Hoe lang ben je? ")
+gewicht = input("Hoeveel weeg je")
