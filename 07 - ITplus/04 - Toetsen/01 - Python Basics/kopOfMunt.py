5eff82f2bc9da9a8618805bede4c007ccf70ed38
Koubyte <info@koubyte.be>
2025-10-03 14:03:54 +0200

    School

--- 07 - ITplus/04 - Toetsen/01 - Python Basics/kopOfMunt.py
+++ 07 - ITplus/04 - Toetsen/01 - Python Basics/kopOfMunt.py
@@ -1,28 +1,42 @@
 import random
 
-keuze = ["kop", "munt"]
-randomKeuze = random.choice(keuze)
 aantalPuntenSpeler = 0
 aantalPuntenComputer = 0
 ronde = 1
 
-print ("--- RONDE " + str(ronde) + " ---")
+print("De computer gooide munt.")
 
-jouwKeuze = input("Denk je dat de computer kop of munt zal gooien?")
+while aantalPuntenSpeler < 5 and aantalPuntenComputer < 5:
+    print("--- RONDE " + str(ronde) + " ---")
+    
+    jouwKeuze = input("Denk je dat de computer kop of munt zal gooien? ")
+    
+    computerKeuze = random.choice(["kop", "munt"])
+    
+    print("De computer gooide " + computerKeuze + ".")
+    
+    if jouwKeuze == computerKeuze:
+        aantalPuntenSpeler += 1
+    else:
+        aantalPuntenComputer += 1
+    
+    print("Jij hebt " + str(aantalPuntenSpeler) + " punten.")
+    print("De computer heeft " + str(aantalPuntenComputer) + " punten.")
+    print("")
+    
+    ronde += 1
 
-print ("De computer gooide " + randomKeuze)
+print("----------------")
+print("EINDE SPEL")
 
-if jouwKeuze == randomKeuze:
-  aantalPuntenSpeler += 1
-else:
-  aantalPuntenComputer += 1
+uitslag = input("Wil je weten wie gewonnen is? ")
 
-print ("Jij hebt " + str(aantalPuntenSpeler) + " punten.")
-print ("De computer heeft " + str(aantalPuntenComputer) + " punten.")
-
-if aantalPuntenSpeler ^ aantalPuntenSpeler == 5 :
-  print ("----------------")
-  print ("EINDE SPEL")
+if uitslag == "ja":
+    if aantalPuntenSpeler == 5:
+        print("Jij bent gewonnen :)")
+    else:
+        print ("")
+        print("De computer is gewonnen :(")
 
     
 
